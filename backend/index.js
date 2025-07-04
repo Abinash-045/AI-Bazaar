5808316d722942852e34951df811df8663f79cbb
Abinash behera <131406513+Abinash-045@users.noreply.github.com>
2025-07-05 00:50:38 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
- origin:["https://ai-bazaar.onrender.com" , "http://localhost:5174"],
+ origin:["https://ai-bazaar.onrender.com" , "https://ai-bazaar-admin.onrender.com"],
  credentials:true
 }))
 
